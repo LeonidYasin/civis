fa34e95dea817a151510f57c8a13630f86924b06
Leonid <79429232+LeonidYasin@users.noreply.github.com>
2026-09-06 09:18:20 +0300

    Create language.py with language selection handler

---
+++ civis_bot/handlers/language.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+"""
+Language selection handler for Civis bot.
+"""
+
+import logging
+
+from telebot.types import Message, ReplyKeyboardRemove
+
+from database import get_user, save_user, get_session, set_session, clear_session
+from locales import TEXTS
+from keyboards import get_main_keyboard, get_language_keyboard
+
+logger = logging.getLogger(__name__)
+
+# Global bot reference
+bot = None
+
+def set_bot(bot_instance):
+    global bot
+    bot = bot_instance
+
+def handle_language_selection(message: Message):
+    """Handle language selection"""
+    tg_id = message.from_user.id
+    text = message.text
+    
+    lang = 'en' if text == "English" else 'ru'
+    
+    user = get_user(tg_id)
+    if user:
+        save_user(tg_id, user.get('username', 'unknown'), language=lang)
+    else:
+        save_user(tg_id, message.from_user.username or "unknown", language=lang)
+    
+    state, _ = get_session(tg_id)
+    if state == 'language_select':
+        clear_session(tg_id)
+    
+    user = get_user(tg_id)
+    if user and user.get('status') == 'completed':
+        bot.reply_to(
+            message,
+            TEXTS[lang]['language_changed'] + "\n\n" + TEXTS[lang]['welcome_citizen'].format(name=user.get('name', '')),
+            reply_markup=get_main_keyboard(lang)
+        )
+    else:
+        bot.reply_to(
+            message,
+            TEXTS[lang]['language_set'] + "\n\n" + TEXTS[lang]['welcome'],
+            reply_markup=ReplyKeyboardRemove()
+        )
+        
+        set_session(tg_id, 'survey_name', {'language': lang})
+        bot.send_message(tg_id, TEXTS[lang]['name_ask'])
